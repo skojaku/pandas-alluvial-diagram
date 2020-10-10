f85e9f8afd18b6923314c64c1a65c2fa3f9bce16
Sadamori Kojaku <skojaku@iu.edu>
2020-10-10 10:48:32 -0400

    add color setting

--- allu_pandas/draw.py
+++ allu_pandas/draw.py
@@ -3,50 +3,92 @@ import matplotlib.pyplot as plt
 from . import utils
 
 
-def draw(df, columns, color=None, ax=None, margin=0.05, row_order=[]):
+def draw(df, columns, color=None, palette=None, ax=None, margin=0.05, row_order=[]):
+    """
+    Draw alluvial diagram from pandas. 
+    This function draws an alluvial diagram, with flows between groups specified by columns of `df`.
+    Specifically, each group is a set of records with the same column (say `col`) and value in the column (say `v`).
+    The groups are orderd horizontally based on `col` and vertically based on `v`.
+    The height of a group represents the number of records that the group contains.
+
+    Params
+    ------
+    df: pandas.DataFrame
+    columns: list of str
+        Name of columns
+    color: str or func
+        Name of color of a function for each flow across multiple groups.
+        A flow starts from the left-most group, go through middle groups and end at the right-most group. 
+        If a function is given, the function will receive a list of (`col`, `v`) pairs in order of 
+        the groups along the path. For example, if a flow goes along a path of groups, ('col1', 'a'), ('col2', 'x'), ('col3', 'W'), 
+        then the function will receive list [('col1', 'a'), ('col2', 'x'), ('col3', 'W')].
+    palette: str
+        Palette of colors. Flow will be colored based on the left-most group.
+    ax: matplotlib.pyplo.gca
+    margin: vertical margin between groups
+    row_order: list of tuples
+        Each tuple is (`col`, `v`), where `col` is the column name and `v` is the value of the group. 
+        Force a group with row_order[i] to be placed above row_order[i+1]
+    """
     if ax is None:
         ax = plt.gca()
 
+    #
+    # Compute the size of groups
+    #
     dg = df[columns].groupby(columns).size().reset_index().sort_values(by=columns)
     sum_all = dg[0].sum()
+
+    #
+    # Compute the top left coner of groups
+    #
     offset = {}
     for col in columns:
         keys = dg[col].drop_duplicates().values
         offset_key = 0.0
 
-        ordered_keys = [r for r in row_order if r in keys]
-        keys = np.concatenate([ordered_keys, keys[~np.isin(keys, row_order)]])
+        col_row_order = [r[1] for r in row_order if r[0] == col]
+        ordered_keys = [r for r in col_row_order if r in keys]
+        keys = np.concatenate([ordered_keys, keys[~np.isin(keys, col_row_order)]])
 
         for i, key in enumerate(keys):
             offset[(col, key)] = -offset_key
             v = np.sum(dg.loc[dg[col] == key, 0]) / sum_all
             offset_key += v + margin
 
-    def calc_order(row, columns, row_order):
-        s = 0
-        for i, col in enumerate(columns):
-            rank = np.where(np.isin(row_order, row[col]))[0][0]
-            if rank is None:
-                rank = len(row_order) + 1
-            s += rank * np.power(len(columns) - i - 1, 10)
-        return s
+    #
+    # Order group vertically
+    #
+    dg = utils.order_groups_by_rows(dg, columns, row_order)
 
-    if len(row_order) > 0:
-        dg["_order"] = dg.apply(lambda x: calc_order(x, columns, row_order), axis=1)
-        dg = dg.sort_values(by="_order")
+    #
+    # Set the colors
+    #
+    palette = utils.make_color_palette(dg, columns, color, palette)
+
+    #
+    # Draw
+    #
     for _, row in dg.iterrows():
         height = row[0]
+        # make a list of (x,y) going through the top left corner of the group
         xys = []
         for i, col in enumerate(columns):
             xys += [(i / (len(columns) - 1), offset[(col, row[col])])]
             offset[(col, row[col])] = offset[(col, row[col])] - height / sum_all
 
-        if callable(color):
-            c = color(row)
+        # generate color
+        if callable(palette):
+            c = palette([(col, row[col]) for col in columns])
         else:
-            c = color
-        utils.plot_curves(
-            xys, height / sum_all, ax, color=c if c is not None else "#444444"
-        )
+            c = palette
+
+        # Draw bezier curve
+        utils.plot_curves(xys, height / sum_all, ax, color=c)
+
+    #
+    # Prettify
+    #
     ax.axis("off")
+
     return ax
--- allu_pandas/utils.py
+++ allu_pandas/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy.special import comb
 
@@ -79,46 +80,38 @@ def plot_curves(xys, height, ax, nTimes=100, r=0.3, color="red"):
         plot_curve([xys[i - 1], xys[i]], height, ax, nTimes, r, color)
 
 
-def draw(df, columns, color, ax=None, margin=0.05, row_order=[]):
-    if ax is None:
-        ax = plt.gca()
-
-    dg = df[columns].groupby(columns).size().reset_index().sort_values(by=columns)
-    sum_all = dg[0].sum()
-    offset = {}
-    for col in columns:
-        keys = dg[col].drop_duplicates().values
-        offset_key = 0.0
-
-        ordered_keys = [r for r in row_order if r in keys]
-        keys = np.concatenate([ordered_keys, keys[~np.isin(keys, row_order)]])
-
+def make_color_palette(dg, columns, color, palette):
+    if (color is None) and (palette is not None):
+        cmap = plt.get_cmap(palette)
+        cols = []
+        for i in range(cmap.N):
+            rgb = cmap(i)[:3]  # will return rgba, we take only first 3 so we get rgb
+            cols += [matplotlib.colors.rgb2hex(rgb) + "aa"]
+        keys = dg[columns[0]].drop_duplicates().values
+        cmap = {}
         for i, key in enumerate(keys):
-            offset[(col, key)] = -offset_key
-            v = np.sum(dg.loc[dg[col] == key, 0]) / sum_all
-            offset_key += v + margin
+            colid = i % len(cols)
+            cmap[key] = cols[colid]
+        color = lambda x: cmap[x[0][1]]
+    elif color is None:
+        color = "#cdcdcdaa"
+    return color
 
+
+def order_groups_by_rows(dg, columns, row_order):
     def calc_order(row, columns, row_order):
         s = 0
         for i, col in enumerate(columns):
-            rank = np.where(np.isin(row_order, row[col]))[0][0]
-            if rank is None:
-                rank = len(row_order) + 1
-            s += rank * np.power(len(columns) - i - 1, 10)
+            col_row_order = [r[1] for r in row_order if r[0] == col]
+            rank = np.where(np.isin(col_row_order, row[col]))[0]
+            if len(rank) == 0:
+                rank = len(col_row_order) + 1
+            else:
+                rank = rank[0]
+            s += rank * np.power(len(columns) - i - 1, 100)
         return s
 
-    dg["_order"] = dg.apply(lambda x: calc_order(x, columns, row_order), axis=1)
-    for _, row in dg.sort_values(by="_order").iterrows():
-        height = row[0]
-        xys = []
-        for i, col in enumerate(columns):
-            xys += [(i / (len(columns) - 1), offset[(col, row[col])])]
-            offset[(col, row[col])] = offset[(col, row[col])] - height / sum_all
-
-        if callable(color):
-            c = color(row)
-        else:
-            c = color
-        plot_curves(xys, height / sum_all, ax, color=c)
-
-    ax.axis("off")
+    if len(row_order) > 0:
+        dg["_order"] = dg.apply(lambda x: calc_order(x, columns, row_order), axis=1)
+        dg = dg.sort_values(by="_order")
+    return dg
